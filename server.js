decd5738ecf32607ad75c6e9217ec0816f192484
Ajay <ajaysengar831@gmail.com>
2025-05-03 07:13:53 -0700

    add code

--- server.js
+++ server.js
@@ -20,16 +20,14 @@ app.get('/', (req, res) => {
   res.send('Server working properly');
 });
 
-// MongoDB connection (only connect once)
-let conn = null;
-const connectToDb = async () => {
-  if (!conn) {
-    conn = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    await conn;
-    console.log('Connected to MongoDB');
-  }
-};
-connectToDb().catch(err => console.log('Error connecting to MongoDB:', err));
+
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  
+}).then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
 // Routes
 app.use('/api', userRoutes);
